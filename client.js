fb8ae0a928279370d5d7c3828e4434a854a0bf93
Jeroen Houben <jeroen@supercool.nl>
2015-12-11 13:06:38 +0100

    wip

--- client.js
+++ client.js
@@ -1,10 +1,38 @@
+function processArticles($articles, emoValue) {
+    // $articles.each(function () {
+    //
+    //
+    //
+    // });
+}
+
+function mediatedValue(val) {
+    var newVal;
+    
+    if (val >= 1) {
+        newVal = 1;
+    } else if (val >= 0.5) {
+        newVal = 0.5;
+    } else if (val >= 0) {
+        newVal = 0;
+    } else if (val >= -0.5) {
+        newVal = -0.5;
+    } else {
+        newVal = -1;
+    }
+}
+
 $(document).ready(function () {
 
     ws = new WebSocket("ws://localhost:9292");
     
     var $placeholder = $("textarea").first();
     
+    var $articles = $('.artikel');
+    
     function processFaceReaderOutput(obj) {
+        console.log(obj);
+        
         var emotions = obj["Classification"]["ClassificationValues"]["ClassificationValue"];
     
         var valenceObj = _.find(emotions, function(emo) {
@@ -14,6 +42,10 @@ $(document).ready(function () {
         var val = parseFloat(valenceObj.Value.float.substring(0, 6));
         
         $placeholder.text(val);
+        
+        val = mediatedValence(val);
+        
+        processArticles($articles, val);
     }
 
     ws.onopen = function() {
